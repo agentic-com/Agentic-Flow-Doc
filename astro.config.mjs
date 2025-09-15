986b85ef7de7db01c8f1d47ffcde4c277b56d396
Kwengang Tankeu <dannickstark@gmail.com>
2025-09-15 23:45:08 +0200

    add topic classification

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,54 +3,95 @@ import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 
 import tailwindcss from "@tailwindcss/vite";
+import starlightSidebarTopics from "starlight-sidebar-topics";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     starlight({
       title: "Agentic Flow",
-	  description: "Agentic Flow is a framework for building AI-powered workflows that runs inside your Browser.",
-	  logo: {
-        src: './src/assets/logo-beige.png',
+      description:
+        "Agentic Flow is a framework for building AI-powered workflows that runs inside your Browser.",
+      logo: {
+        src: "./src/assets/logo-beige.png",
       },
-	  favicon: './src/assets/logo.png',
+      favicon: "./src/assets/logo.png",
       social: [
-        { icon: 'codeberg', label: 'Codeberg', href: 'https://codeberg.org/knut' },
-        { icon: 'discord', label: 'Discord', href: 'https://astro.build/chat' },
-        { icon: 'github', label: 'GitHub', href: 'https://github.com/withastro' },
-        { icon: 'gitlab', label: 'GitLab', href: 'https://gitlab.com/delucis' },
-        { icon: 'mastodon', label: 'Mastodon', href: 'https://m.webtoo.ls/@astro' },
-      ],
-      sidebar: [
         {
-          label: "Guides",
-          items: [
-            // Each item here is one entry in the navigation menu.
-            { label: "Example Guide", slug: "guides/example" },
-          ],
+          icon: "codeberg",
+          label: "Codeberg",
+          href: "https://codeberg.org/knut",
         },
+        { icon: "discord", label: "Discord", href: "https://astro.build/chat" },
         {
-          label: "Reference",
-          autogenerate: { directory: "reference" },
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/withastro",
+        },
+        { icon: "gitlab", label: "GitLab", href: "https://gitlab.com/delucis" },
+        {
+          icon: "mastodon",
+          label: "Mastodon",
+          href: "https://m.webtoo.ls/@astro",
         },
       ],
-	  defaultLocale: 'root',
+      defaultLocale: "root",
       locales: {
         // English docs in `src/content/docs/en/`
         root: {
-          label: 'English',
-		  lang: 'en',
+          label: "English",
+          lang: "en",
         },
-		fr: {
-          label: 'Français',
+        fr: {
+          label: "Français",
         },
       },
-      plugins: [],
+      plugins: [
+        starlightSidebarTopics([
+          {
+            label: "Workflow",
+            link: "/workflow/guides/example/",
+            icon: "open-book",
+			items: [
+				{
+					label: "Guides",
+					autogenerate: { directory: "workflow/guides" },
+				  },
+				  {
+					label: "Reference",
+					autogenerate: { directory: "workflow/reference" },
+				  },
+			],
+          },
+          {
+            label: {
+				en: "Marketplace",
+				fr: "Marché",
+			},
+            link: "/marketplace/guides/example/",
+            icon: "information",
+			items: [
+				{
+					label: "Guides",
+					autogenerate: { directory: "marketplace/guides" },
+				  },
+				  {
+					label: "Reference",
+					autogenerate: { directory: "marketplace/reference" },
+				  },
+			],
+          },
+        ]),
+      ],
+	  components: {
+        // Override the default `Sidebar` component with a custom one.
+        Sidebar: './src/components/Sidebar.astro',
+      },
       customCss: [
         // Path to your Tailwind base styles:
         "./src/styles/global.css",
       ],
-	  lastUpdated: true,
+      lastUpdated: true,
     }),
   ],
 
