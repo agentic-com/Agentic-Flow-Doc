fa7643f3c14deb43a185dae8b2959628b58e20ff
Kwengang Tankeu <dannickstark@gmail.com>
2025-09-15 21:58:45 +0200

    add tailwind + global colors

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,11 +1,9 @@
 import { docsSchema } from '@astrojs/starlight/schema';
 import { defineCollection } from 'astro:content';
-import { pageThemeObsidianSchema } from 'starlight-theme-obsidian/schema';
 
 export const collections = {
     docs: defineCollection({
         schema: docsSchema({
-            extend: pageThemeObsidianSchema
         })
     })
 };
